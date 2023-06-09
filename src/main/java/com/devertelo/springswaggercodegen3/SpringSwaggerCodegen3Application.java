5b9ae25e3cd09f1a2a17b46f1b4173cd51fc74c4
João Vertelo <joaovertelo@gmail.com>
2023-06-09 20:08:03 -0300

    build(spring-swagger-codegen-3): add dependencies and configs

---
+++ src/main/java/com/devertelo/springswaggercodegen3/SpringSwaggerCodegen3Application.java
@@ -0,0 +1,13 @@
+package com.devertelo.springswaggercodegen3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerCodegen3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwaggerCodegen3Application.class, args);
+	}
+
+}
