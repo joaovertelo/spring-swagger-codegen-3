c4bb686034ee9525f954add0640f9e9523a03d2a
João Vertelo <joaovertelo@gmail.com>
2023-06-11 14:49:15 -0300

    refactor: change api definitions

--- src/main/java/com/devertelo/springswaggercodegen3/controller/UsersController.java
+++ src/main/java/com/devertelo/springswaggercodegen3/controller/UsersController.java
@@ -1,7 +1,8 @@
 package com.devertelo.springswaggercodegen3.controller;
 
 import com.devertelo.springswaggercodegen3.api.UsersApi;
-import com.devertelo.springswaggercodegen3.model.User;
+import com.devertelo.springswaggercodegen3.model.UserRequest;
+import com.devertelo.springswaggercodegen3.model.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,43 +15,59 @@ import java.util.UUID;
 @RestController
 public class UsersController implements UsersApi {
 
-    private Map<String, User> users = new HashMap<>();
+    private Map<String, UserResponse> users = new HashMap<>();
 
     @Override
-    public ResponseEntity<User> createUser(User user) {
-        user.setId(UUID.randomUUID().toString());
-        user.setCreatedAt(OffsetDateTime.now());
-        users.put(user.getId(), user);
-        return ResponseEntity.ok(user);
+    public ResponseEntity<UserResponse> createUser(UserRequest user) {
+        var newUser = toUserResponse(user);
+        users.put(newUser.getId(), newUser);
+        return ResponseEntity.ok(newUser);
     }
 
     @Override
     public ResponseEntity<Void> deleteUser(String id) {
-        User user = users.remove(id);
+        UserResponse user = users.remove(id);
         return user != null ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
     @Override
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> userList = users.values()
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        List<UserResponse> allUsers = this.users.values()
                 .stream()
                 .toList();
-        return ResponseEntity.ok(userList);
+        return ResponseEntity.ok(allUsers);
     }
 
     @Override
-    public ResponseEntity<User> getUserById(String id) {
+    public ResponseEntity<UserResponse> getUserById(String id) {
         var user = users.get(id);
         return ResponseEntity.ok(user);
     }
 
     @Override
-    public ResponseEntity<User> updateUser(String id, User user) {
-        if (users.containsKey(id)) {
-            user.setId(id);
-            users.put(id, user);
-            return ResponseEntity.ok(user);
+    public ResponseEntity<UserResponse> updateUser(String id, UserRequest userRequest) {
+
+        var userById = users.get(id);
+
+        if (userById != null) {
+            var newUser = toUserResponse(userRequest);
+            newUser.setId(userById.getId());
+            newUser.setCreatedAt(userById.getCreatedAt());
+            users.put(id, newUser);
+            return ResponseEntity.ok(newUser);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    public UserResponse toUserResponse(UserRequest request) {
+        return new UserResponse()
+                .id(UUID.randomUUID().toString())
+                .createdAt(OffsetDateTime.now())
+                .username(request.getUsername())
+                .name(request.getName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .status(request.getStatus())
+                .password(request.getPassword());
     }
 }
